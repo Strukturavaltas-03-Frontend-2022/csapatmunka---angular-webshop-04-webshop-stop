12a66f32abfef62127452eec4ad58313ef762fc4
Joe <reszketo.robert@gmail.com>
2022-11-04 17:49:14 +0100

    beta structure

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -14,9 +14,11 @@ export class ConfigService {
   appName: string = 'Sample Webshop';
 
   menuItems: IMenuItem[] = [
-    {text: 'Home', link: '/', icon: 'home'},
-    {text: 'Category01', link: '/cat01'},
-    {text: 'Category02', link: '/cat02'},
+    { text: 'Home', link: '/', icon: 'home' },
+    { text: 'Category01', link: '/cat01' },
+    { text: 'Category02', link: '/cat02' },
+    { text: 'Category03', link: '/cat03' },
+    { text: 'Category04', link: '/cat04' },
   ];
 
   constructor() { }
