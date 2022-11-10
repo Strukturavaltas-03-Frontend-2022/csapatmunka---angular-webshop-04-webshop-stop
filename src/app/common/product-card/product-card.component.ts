2f30fb6cc6cc7373385e6012a59fe3fbf58c70a6
Joe <reszketo.robert@gmail.com>
2022-11-10 20:43:12 +0100

    kártyának objektum

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -7,15 +7,22 @@ import { Product } from 'src/app/model/classes/product';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-  @Input() oneProduct: Product = ''
-  @Input() cardImageUrl: string = 'https://m.media-amazon.com/images/I/919XM42JQlL.jpg'
-  @Input() cardTitle: string = 'A long-ass text as a title placeholder'
-  @Input() cardAuthor: string = 'G. R. R. R. Rowling'
+  @Input() oneProduct: Product = {
+    id: 1,
+    catId: 3,
+    name: 'Things Fall Apart',
+    author: 'Chinua Achebe',
+    description: 'https://en.wikipedia.org/wiki/Things_Fall_Apart',
+    image: 'https://m.media-amazon.com/images/I/919XM42JQlL.jpg',
+    price: 6.99,
+    stock: 0,
+    featured: true,
+    active: false
+  };
   @Input() cardDescription: string = `An even longer text, because that's how descriptions are.`
-  @Input() stockStatus: number = 15
-  @Input() priceStatus: number = 15.99
   @Input() stockText: string = 'Stock:'
   @Input() priceText: string = 'Price:'
+
 
   constructor() { }
 
