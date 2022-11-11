9e5de998b2f312916443acd92261d4b9948b6fb9
Mónika <gergye.monika@gmail.com>
2022-11-11 17:42:12 +0100

    Teszt divek megjelennek kártyákként, Adatkötések előkészítve, Lapozó gombok emittálnak

--- src/app/common/carousel/carousel.component.ts
+++ src/app/common/carousel/carousel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Product } from 'src/app/model/classes/product';
 
 @Component({
   selector: 'app-carousel',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
+  @Output() leftClick:EventEmitter<boolean>=new EventEmitter();
+  @Output() rightClick:EventEmitter<boolean>=new EventEmitter();
+
+  @Input()  productList:Product[]=[]
+
+  test_array:string[]=['a','b','c','d','e'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLeftClick()
+  {
+    this.leftClick.emit(true);
+    //console.log('left')
+  }
+
+  onRightClick()
+  {
+    this.rightClick.emit(true);
+   // console.log('right')
   }
 
 }
